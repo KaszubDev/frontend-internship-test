c4b4f2657d9d7dbda721e7ab1beec29097e7c59b
Krzysztof <kaszubowskikrzys@gmail.com>
2019-01-23 15:50:22 +0100

    Task 3 & Improved Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,39 @@
 /*eslint-env browser*/
 document.getElementById("show-popup-form").addEventListener("click", showPopup);
 document.getElementById("closeButton").addEventListener("click", closePopup);
+document.getElementById("submitBtn").addEventListener("click", disappearOnTime);
+
+var check_checkbox = false;
+var email_ok = false;
+var passwd_ok = false;
+document.getElementById("checkbox_1").addEventListener("click", change);
+
+function change(){
+    check_checkbox = !check_checkbox;
+    if((check_checkbox === true) && (email_ok === true) && (document.getElementById("passw").value.length != 0) && (document.getElementById("email").value.length != 0) && (passwd_ok === true)){
+        document.getElementById("submitBtn").disabled = false;
+    }
+    else document.getElementById("submitBtn").disabled = true;
+}
+
 function showPopup(){
     document.getElementById("login").style.display = 'block';
 }
 function closePopup(){
     document.getElementById("login").style.display = 'none';
+}
+function disappear() {
+    closePopup();
+    document.getElementById("show-popup-form").style.display = "none";
+    document.getElementById("message").style.display = "block";
+    window.stop();
+}
+
+function disappearOnTime(){
+    if(check_checkbox === true){
+    setTimeout(disappear, 3000);
+    }
+    else return false;
 }
 
 document.querySelector("#email").addEventListener('change', function() {
@@ -16,6 +44,7 @@ document.querySelector("#email").addEventListener('change', function() {
         this.select();
     } else {
         this.classList.remove("error");
+        email_ok = true;
     }
 });
 
@@ -27,5 +56,6 @@ document.querySelector("#passw").addEventListener('change', function() {
     }
     else {
         this.classList.remove("error");
+        passwd_ok = true;
     }
 });
