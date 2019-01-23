0dc024456d32546f2978e397acc41176f26d6d05
Krzysztof <kaszubowskikrzys@gmail.com>
2019-01-23 14:28:06 +0100

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,25 @@ function showPopup(){
 function closePopup(){
     document.getElementById("login").style.display = 'none';
 }
+
+document.querySelector("#email").addEventListener('change', function() {
+    var reg = new RegExp('^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$', 'i');
+    if (!reg.test(this.value)) {
+        this.classList.add("error");
+        alert("Podaj adres e-mail w prawidłowym formacie np. john.smith@example.com");
+        this.select();
+    } else {
+        this.classList.remove("error");
+    }
+});
+
+document.querySelector("#passw").addEventListener('change', function() {
+    if(this.value.length < 6) {
+        this.classList.add("error");
+        alert("Podaj prawidłowe hasło. Hasło musi mieć przynajmniej 6 znaków.");
+        this.select();
+    }
+    else {
+        this.classList.remove("error");
+    }
+});
